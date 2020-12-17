a38d426fc7644c3c462d0ad3b536d30e11a7f104
rowleya-phut <andrew.rowley@porthosp.nhs.uk>
2020-12-17 10:46:35 +0000

    Moved convertUnix function to separate file

--- scripts/sendRoom.js
+++ scripts/sendRoom.js
@@ -42,44 +42,6 @@ $( document ).ready(function() {
 
     var url = "php/getFormData.php";
 
-    //convert UNIX time to human readable string
-    function convertUnix(unixTime){
-        var string = "";
-        var dateCompleted = new Date(unixTime*1000);
-        var minute = dateCompleted.getMinutes();
-        var hour = dateCompleted.getHours();
-        
-        var weekday = new Array(7);
-        weekday[0] = "Sunday";
-        weekday[1] = "Monday";
-        weekday[2] = "Tuesday";
-        weekday[3] = "Wednesday";
-        weekday[4] = "Thursday";
-        weekday[5] = "Friday";
-        weekday[6] = "Saturday";
-        var day = weekday[dateCompleted.getDay()];
-        var date = dateCompleted.getDate();
-        
-        var monthNames = new Array(12)
-        monthNames[0] = "Jan";
-        monthNames[1] = "Feb";
-        monthNames[2] = "Mar";
-        monthNames[3] = "Apr";
-        monthNames[4] = "May";
-        monthNames[5] = "June";
-        monthNames[6] = "July";
-        monthNames[7] = "Aug";
-        monthNames[8] = "Sept";
-        monthNames[9] = "Oct";
-        monthNames[10] = "Nov";
-        monthNames[11] = "Dec";
-        var month = monthNames[dateCompleted.getMonth()];
-
-        var year = dateCompleted.getFullYear();
-        string += day + " "+ date + " " + month + "  " + year + ": " + hour + ":" + minute;
-        return string;
-    }
-
     //function make the ajax call to the server
     //refactored to a function as it is called multiple times
     function AjaxCall(){
